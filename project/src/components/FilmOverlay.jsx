e35b296009bbf19d1b338e3148860fab537e07dc
warongkorn6560 <warongkorn6560@gmail.com>
2024-07-25 14:13:40 +0700

    :alien: Update

--- project/src/components/FilmOverlay.jsx
+++ project/src/components/FilmOverlay.jsx
@@ -1,8 +1,8 @@
-export default function FilmOverlay() {
+export default function FilmOverlay({ addedClass }) {
   return (
     <img
       src="/film-grain.png"
-      className="absolute w-full h-full top-0 left-0 object-cover mix-blend-soft-light z-0 pointer-events-none"
+      className={`absolute w-full h-full top-0 left-0 object-cover mix-blend-soft-light z-0 pointer-events-none ${addedClass}`}
     />
   );
 }
--- project/src/components/LoadingStage.jsx
+++ project/src/components/LoadingStage.jsx
@@ -29,55 +29,40 @@ export default function LoadingScreen({
   }, [isActive]);
 
   return (
-    <div
-      id={id}
-      className={`absolute bottom-0 left-0 h-screen w-full bg-[#FAF460] bg-contain flex flex-col pt-16 ${className}`}
-    >
-      <div className="absolute bottom-0 left-0 h-screen w-full bg-[url('/bg/LoadingBG.png')] bg-contain flex flex-col pt-16">
-        {/* header */}
-        <div className="px-5">
-          <SiteLogo />
-          <FilmOverlay />
-        </div>
-
-        <h2 className="pt-16 made-dillan text-[#0A59CE] text-3xl text-center">
-          your soundtrack is
-        </h2>
-        <h2 className="made-dillan text-[#0A59CE] text-3xl text-center">
-          on the way...
-        </h2>
-        {/* vinyl image */}
-        <img
-          className="mt-20 place-self-center w-[80%] object-contain"
-          src="/vinyl.png"
-        />
-        {/* loading progress bar */}
-        <div className="relative mx-20 h-1 bg-zinc-300 mt-12">
-          <div
-            id="pregress-bar"
-            className="absolute h-full w-60 left-0 bg-zinc-700 rounded-full"
+    <div id={id} className={`bg-[#FAF460] ${className}`}>
+      <div className="px-5 h-full">
+        <FilmOverlay addedClass="!opacity-30 !mix-blend-normal" />
+        <SiteLogo />
+        {/* State */}
+        <div className="py-12 flex flex-col items-center justify-center space-y-8 h-full">
+          {/* Title */}
+          <h2 className="made-dillan text-[#0A59CE] text-3xl text-center">
+            <span>your soundtrack is</span>
+            <br></br>
+            <span className="made-dillan text-[#0A59CE] text-3xl text-center">
+              on the way...
+            </span>
+          </h2>
+          {/* vinyl image */}
+          <img
+            className="place-self-center w-[80%] object-contain"
+            src="/vinyl.png"
           />
+          {/* loading bar */}
+          <div className="flex flex-col items-center w-full px-8">
+            {/* loading progress bar */}
+            <div className="relative h-1 bg-zinc-300 w-full">
+              <div
+                id="pregress-bar"
+                className="absolute h-full w-60 left-0 bg-zinc-700 rounded-full"
+              />
+            </div>
+            {/* Loading number */}
+            <h2 className="made-dillan text-[#0A59CE] text-3xl text-center mt-4">
+              {loadingPercentage}%
+            </h2>
+          </div>
         </div>
-        {/* Loading number */}
-        <h2 className="made-dillan text-[#0A59CE] text-3xl text-center mt-4">
-          {loadingPercentage}%
-        </h2>
-      </div>
-      {/* back button */}
-      <button
-        onClick={onBack}
-        className="absolute bottom-8 p-5 flex space-x-3 items-center"
-      >
-        <img className="w-8 rotate-180" src="/arrow2.svg" />
-        <span className="font-medium text-black"> back </span>
-      </button>
-
-      {/* footer & buttons */}
-      <div className="absolute bottom-6 right-0">
-        <button onClick={onNext} className="p-5 flex space-x-3 items-center">
-          <div className="border-2 border-white rounded-full w-11 h-11" />
-          <img className="absolute left-4" src="/arrow8.svg" />
-        </button>
       </div>
     </div>
   );
