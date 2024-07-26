26cfd139a25e8ad005d589090504b5fc88208a2a
warongkorn6560 <warongkorn6560@gmail.com>
2024-07-26 10:25:08 +0700

    :alien: Update

--- project/src/components/resultScreen.jsx
+++ project/src/components/resultScreen.jsx
@@ -87,6 +87,7 @@ export default function ResultScreen({
 
   // screen capture
   function captureToFile() {
+    console.log("click");
     domtoimage
       .toBlob(document.querySelector(".capture-screen"))
       .then((blob) => {
@@ -130,7 +131,7 @@ export default function ResultScreen({
   return (
     <div
       id={id}
-      className={`absolute bottom-0 left-0 h-screen w-full bg-[#155FCB] ${className}`}
+      className={`capture-screen absolute bottom-0 left-0 h-screen w-full bg-[#155FCB] ${className}`}
     >
       {/* <button onClick={captureToFile}>share result to instagram</button> */}
       {socials.map((social, index) => (
